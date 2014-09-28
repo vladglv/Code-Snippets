9cb08447b8c99afd4a9e40e4f7c3c6750a0489c2
vladglv <vladgulevich@hotmail.com>
2014-09-28 16:13:53 -0400

    Update BinaryToBase10.java

--- BinaryToBase10.java
+++ BinaryToBase10.java
@@ -2,15 +2,14 @@ import java.util.Scanner;
 
 public class Base2toBase10 {
 	
-	public static void main(String[] args) {
-
-        String input = "101010";
+	public static void main(String[] args) 
+	{
+		String input = "101010";
 		System.out.println("The binary number " + input + " is " + BinaryToNumber(input) + " in base 10.");
-
 	}
 	
-	public static int BinaryToNumber(String num) {
-
+	public static int BinaryToNumber(String num) 
+	{
 		int res = 0;
 		for (int i = num.length() - 1, pot = 1; i >=0; i--)
 			res |= ((num.charAt(i) == '1') ? (pot << i) : 0);
