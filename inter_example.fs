aad8791d72ff3208b6a55521169bba917e5f5be8
vlad_glv <vlad_glv@home>
2016-01-24 10:11:25 -0500

    Added perm example and reformatted inter example

--- inter_example.fs
+++ inter_example.fs
@@ -4,8 +4,8 @@ let rec inter e l =
     match l with
     | [] -> [ [ e ] ]
     | x :: xs -> (e :: x :: xs) :: List.map (fun u -> x :: u) (inter e xs)
-        
-let test_list = [ 1 .. 1 .. 16 ]
+
+let test_list = [ 1..1..16 ]
 let test_item = 0
 let test_rslt = (inter test_item test_list)
 
---
+++ perm_example.fs
@@ -0,0 +1,16 @@
+﻿module permute
+
+let rec perm l = 
+    match l with
+    | [] -> [ [] ]
+    | x :: xs -> List.concat (List.map (interleave.inter x) (perm xs))
+
+let test_list0 = []
+let test_list1 = [ 1..1..1 ]
+let test_list2 = [ 1..1..2 ]
+let test_list3 = [ 1..1..3 ]
+
+printf "%A\n" (perm test_list0)
+printf "%A\n" (perm test_list1)
+printf "%A\n" (perm test_list2)
+printf "%A\n" (perm test_list3)
