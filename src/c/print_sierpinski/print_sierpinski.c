b0b67d9a12ae20e1b8107b844297573bd1506358
Vlad Glv <vladglv@users.noreply.github.com>
2017-06-25 20:32:41 -0400

    Cleaned up Sierpinski code and added a makefile for said code

--- src/c/print_sierpinski/print_sierpinski.c
+++ src/c/print_sierpinski/print_sierpinski.c
@@ -1,8 +1,3 @@
-/**
- * @author VL
- * @date 10/18/2015
- * @file q1b_print_sierpinski.c
- */
 #include <assert.h>
 #include <ctype.h>
 #include <stdio.h>
@@ -23,41 +18,40 @@ typedef struct _display {
     char* buffer; //!< Buffer holding display's data
 } display;
 
-typedef display* display_ptr; //!< Type definition to avoid explicit use of
-                              //!< pointers
+typedef display* display_ptr;
 
 /**
  * @brief Creates a new virtual display
- * @param x size x
- * @param y size y
- * @return NULL or a valid display
+ * @param x Size x
+ * @param y Size y
+ * @return NULL or a valid display address
  */
 display_ptr vl_create_display(int x, int y);
 
 /**
- * @brief Clears display with c
- * @param p_display display
- * @param c fill character
+ * @brief Clears display with supplied character 'c'
+ * @param p_display Display
+ * @param c Fill character
  */
 void vl_clear_display(display_ptr p_display, char c);
 
 /**
- * @brief Prints content of the display to the out stream
- * @param p_display display to print
- * @param out a valid text output stream
+ * @brief Prints contents of the display to the out stream
+ * @param p_display Display to print
+ * @param out A valid text output stream
  */
 void vl_print_display(display_ptr p_display, FILE* out);
 
 /**
- * @brief Frees all resources allocated by the vl_create_display call
- * @param pp_display
+ * @brief Frees all resources allocated by the 'vl_create_display' call
+ * @param pp_display Pointer to the display instance
  */
 void vl_delete_display(display_ptr* pp_display);
 
 /**
  * @brief Draws a triangle of height 'height' at (pos_x, pos_y)
- * @param p_display display to use
- * @param height height of the triangle
+ * @param p_display Display instance
+ * @param height Height of the triangle
  * @param pos_x x-coordinate
  * @param pos_y y-coordinate
  */
@@ -65,8 +59,8 @@ void vl_draw_triangle(display_ptr p_display, int height, int pos_x, int pos_y);
 
 /**
  * @brief Draws a point at (pos_x, pos_y)
- * @param p_display display to use
- * @param c character to draw
+ * @param p_display Display instance
+ * @param c Character to draw
  * @param pos_x x-coordinate
  * @param pos_y y-coordinate
  */
@@ -76,13 +70,10 @@ void vl_draw_point(display_ptr p_display, char c, int pos_x, int pos_y);
 // vl_draw API implementation
 // -----------------------------------------------------------------------------
 
-display_ptr vl_create_display(int x, int y)
-{
-    // - Check the coordinates
+display_ptr vl_create_display(int x, int y) {
     if(x < 1 || y < 1)
         return NULL;
 
-    // - Allocate memory for the display and the virtual screen buffer
     display_ptr m_d = (display_ptr)malloc(sizeof(display));
     if(m_d) {
         m_d->x = x;
@@ -97,46 +88,36 @@ display_ptr vl_create_display(int x, int y)
     return NULL;
 }
 
-void vl_delete_display(display_ptr* pp_display)
-{
-    // - Check the display variable passed in
+void vl_delete_display(display_ptr* pp_display) {
     assert(pp_display != NULL);
     if(!pp_display)
         return;
 
-    // - Check the actual display variable
     assert(*pp_display != NULL);
     if(!*pp_display)
         return;
 
-    // - Free owned resources
     free((*pp_display)->buffer);
     free(*pp_display);
 
     *pp_display = NULL;
 }
 
-void vl_clear_display(display_ptr p_display, char c)
-{
-    // - Check the display variable
+void vl_clear_display(display_ptr p_display, char c) {
     assert(p_display != NULL);
     if(!p_display)
         return;
 
-    // - Iterate over the display and fill the value
     for(int i = 0; i < p_display->x * p_display->y; i++)
         p_display->buffer[i] = c;
 }
 
-void vl_print_display(display_ptr p_display, FILE* out)
-{
-    // - Check the display and the output stream
+void vl_print_display(display_ptr p_display, FILE* out) {
     assert(p_display != NULL);
     assert(out != NULL);
     if(!p_display || !out)
         return;
 
-    // - Print the display to the text stream
     for(int y = 0; y < p_display->y; y++) {
         for(int x = 0; x < p_display->x; x++) {
             assert(y * p_display->x + x < p_display->x * p_display->y);
@@ -146,43 +127,33 @@ void vl_print_display(display_ptr p_display, FILE* out)
     }
 }
 
-void vl_draw_point(display_ptr p_display, char c, int pos_x, int pos_y)
-{
-    // - Check the display
+void vl_draw_point(display_ptr p_display, char c, int pos_x, int pos_y) {
     assert(p_display != NULL);
     if(!p_display)
         return;
 
-    // - Check the coordinates for a lower bound
     if(pos_x < 0 || pos_y < 0)
         return;
 
-    // - Check the coordinates for an upper bound
     if(pos_x > (p_display->x - 1) || pos_y > (p_display->x - 1))
         return;
 
-    // - Draw a point where needed if the position is valid
     assert(pos_y * p_display->x + pos_x < p_display->x * p_display->y);
     if(pos_y * p_display->x + pos_x < p_display->x * p_display->y)
         p_display->buffer[pos_y * p_display->x + pos_x] = c;
 }
 
-void vl_draw_triangle(display_ptr p_display, int height, int pos_x, int pos_y)
-{
-    // - Check the display
+void vl_draw_triangle(display_ptr p_display, int height, int pos_x, int pos_y) {
     assert(p_display != NULL);
     if(!p_display)
         return;
 
-    // - Check the coordinates lower bounds
     if(pos_x < 0 || pos_y < 0)
         return;
 
-    // - Validate the input
     if(height < 1)
         return;
 
-    // - Create all needed variables
     int row_lenght = 2 * height - 1;
     int stars = 0;
     int pos_begin = 0;
@@ -191,15 +162,12 @@ void vl_draw_triangle(display_ptr p_display, int height, int pos_x, int pos_y)
     if((pos_x + row_lenght > p_display->x) && (pos_y + height > p_display->y))
         return;
 
-    // - Draw the triangle
     for(int y = 0; y < height; y++) {
         for(int x = 0; x < row_lenght; x++) {
             stars = 2 * (y + 1) - 1;
             pos_begin = (row_lenght - stars) / 2;
             pos_end = row_lenght - pos_begin;
 
-            // - Draw a start if we are in range of [pos_begin, pos_end[
-            // - or a space otherwise
             if((x >= pos_begin) && (x < pos_end))
                 vl_draw_point(p_display, '*', x + pos_x, y + pos_y);
         }
@@ -211,10 +179,9 @@ void vl_draw_triangle(display_ptr p_display, int height, int pos_x, int pos_y)
 // -----------------------------------------------------------------------------
 
 /**
- * @brief Checks if the string 'p_str' contains all numerical values
- * @param p_str non-null string to check
- * @return 0 if the string does contains only digits and non-zero value
- * otherwise
+ * @brief Checks if the string 'p_str' contains only numerical values
+ * @param p_str Non NULL string to check
+ * @return 0 if the string contains only digits and non-zero value otherwise
  */
 int vl_check_number_string(char* p_str);
 
@@ -222,22 +189,15 @@ int vl_check_number_string(char* p_str);
 // vl_utility API implementation
 // -----------------------------------------------------------------------------
 
-int vl_check_number_string(char* p_str)
-{
+int vl_check_number_string(char* p_str) {
     assert(p_str != NULL);
-
-    // - Check arguments
     if(p_str == NULL)
         return 2;
 
-    // - Validate the string, we expect strings to be null terminated
+    // - Expect a null terminated string
     while(*p_str) {
-        // - Check each character individually
-        if(isdigit(*p_str) == 0)
+        if(isdigit(*(p_str++)) == 0)
             return 1;
-
-        // - Advance the pointer
-        p_str++;
     }
 
     return 0;
@@ -251,21 +211,17 @@ void print_recursive_sierpinski(display_ptr p_display,
                                 int height,
                                 int pos_x,
                                 int pos_y,
-                                int fractal_levels)
-{
-    // - Validate the input
+                                int fractal_levels) {
     if(height < 2 || fractal_levels < 1)
         return;
 
-    // - Divide the triangles
     height /= 2;
     int lenght = 2 * height - 1;
 
-    // - Prepare vertices
-    int triangle_vertices[3][2] = { { pos_x + lenght / 2 + 1, pos_y },
-                                    { pos_x, pos_y + height },
-                                    { pos_x + lenght + 1, pos_y + height } };
-    // - Draw recursively
+    int triangle_vertices[3][2] = {{pos_x + lenght / 2 + 1, pos_y},
+                                   {pos_x, pos_y + height},
+                                   {pos_x + lenght + 1, pos_y + height}};
+
     print_recursive_sierpinski(p_display,
                                height,
                                triangle_vertices[0][0],
@@ -282,7 +238,6 @@ void print_recursive_sierpinski(display_ptr p_display,
                                triangle_vertices[2][1],
                                fractal_levels - 1);
 
-    // - Draw only if the fractal level is 1
     if(fractal_levels == 1) {
         vl_draw_triangle(p_display,
                          height,
@@ -302,14 +257,13 @@ void print_recursive_sierpinski(display_ptr p_display,
 /**
  * @brief Prints a Sierpinski triangle of height 'height' and fractal levels
  * 'fractal_levels'
- * @param display display to draw the triangles to
- * @param height the height of the triangle
- * @param fractal_levels fractal levels
+ * @param display Display instance
+ * @param height Height of the triangle
+ * @param fractal_levels Fractal levels to use
  */
-void
-print_sierpinski_triangle(display_ptr p_display, int height, int fractal_levels)
-{
-    // - Validate the input
+void print_sierpinski_triangle(display_ptr p_display,
+                               int height,
+                               int fractal_levels) {
     if(height < 1 || fractal_levels < 0)
         return;
 
@@ -317,15 +271,13 @@ print_sierpinski_triangle(display_ptr p_display, int height, int fractal_levels)
     if(height % (1 << fractal_levels) != 0)
         return;
 
-    // - Handle the base case here or otherwise call the helper function
     if(fractal_levels == 0)
         vl_draw_triangle(p_display, height, 0, 0);
     else
         print_recursive_sierpinski(p_display, height, 0, 0, fractal_levels);
 }
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
     assert(argc >= 1);
     if(argc < 3) {
         printf("ERROR: Missing arguments!\n");
@@ -376,7 +328,6 @@ int main(int argc, char** argv)
         return EXIT_FAILURE;
     }
 
-    // - Convert the user input to an integer
     int u_height = atoi(argv[1]);
     if(u_height < 1) {
         printf("WARNING: Integer argument is too small!\n");
@@ -386,7 +337,6 @@ int main(int argc, char** argv)
         return EXIT_FAILURE;
     }
 
-    // - Convert the user input to an integer
     int u_fractal = atoi(argv[2]);
     if(u_fractal < 0) {
         printf("WARNING: Integer argument is too small!\n");
@@ -404,7 +354,6 @@ int main(int argc, char** argv)
         return EXIT_FAILURE;
     }
 
-    // - Compute the virtual screen size
     int m_screen_x = 2 * u_height - 1;
     int m_screen_y = u_height;
 
@@ -414,28 +363,18 @@ int main(int argc, char** argv)
         return EXIT_FAILURE;
     }
 
-    // - Create a display
     display_ptr m_screen = vl_create_display(m_screen_x, m_screen_y);
 
-    // - Check if it was created successfully
     if(m_screen == NULL) {
         printf("Failed to allocate memory required for storing virtual screen "
                "space\n");
         return EXIT_FAILURE;
     }
 
-    // - Clear the screen
     vl_clear_display(m_screen, ' ');
-
-    // - Draw Sierpinski triangle to the virtual screen
     print_sierpinski_triangle(m_screen, u_height, u_fractal);
-
-    // - Print the Sierpinski triangle to the standard output stream
     vl_print_display(m_screen, stdout);
-
-    // - Delete the display by freeing the resources it owns
     vl_delete_display(&m_screen);
 
-    // - Return!
     return EXIT_SUCCESS;
 }
